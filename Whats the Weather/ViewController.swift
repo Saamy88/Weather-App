13dff240c2f07e9bc51bf1ee14fac9eab651bae0
Saamy88 <cordova735_2@hotmail.com>
2015-08-14 13:38:38 -0500

    Initial Commit

---
+++ Whats the Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats the Weather
+//
+//  Created by Samuel Cordova on 8/14/15.
+//  Copyright © 2015 Samuel Cordova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
