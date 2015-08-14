139e2d2fa7f1759a1577e9b4b5e3735e15800181
Saamy88 <cordova735_2@hotmail.com>
2015-08-14 18:01:26 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+iOS weather App. Flexible enough to tell you the weather summary of the most important cities of the world
--- Whats the Weather/ViewController.swift
+++ Whats the Weather/ViewController.swift
@@ -10,9 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        var flag = false
+        
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl
+        {
+            
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error ) -> Void in
+                
+                if let urlContent = data
+                {
+                    
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                    
+                    let websiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                    
+                    if websiteArray!.count > 1
+                    {
+                        let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                        
+                        if weatherArray.count > 1
+                        {
+                            flag = true
+                            
+                            let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg", withString: "°")
+                            
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                
+                                self.resultLabel.text = weatherSummary
+                                
+                            })
+                            
+                        }
+                        
+                    }
+                    
+                }
+                if flag == false
+                {
+                    self.resultLabel.text = "Couldn't find that city. Please try again!"
+                }
+            }
+            task?.resume()
+
+        }
+        else
+        {
+            self.resultLabel.text = "Couldn't find that city. Please try again!"
+
+        }
+
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
